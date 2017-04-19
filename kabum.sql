ca012ac1e84ab8aa62e27f8cff44a3960daa6e5f
Raffaela Monteiro <raffaela.monteiro@gmail.com>
2017-04-18 22:32:11 -0300

    Update kabum.sql

--- kabum.sql
+++ kabum.sql
@@ -3,5 +3,5 @@ id integer not null primary key generated always as identity(start with 1, incre
 name varchar(80) not null,
 description varchar (300) not null,
 image varchar(300) not null,
-value decimal 10,2
+value decimal (10,2)
 )
